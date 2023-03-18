de1c97c5bb5029babc0a376e2a86be1016fe1536
Fabian Bergström <fabian@fmbb.se>
2023-03-18 18:52:37 +0100

    Trace a sphere (#3)
    * a sphere appears
    
    * use all the colors
    
    * add screenshot

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,108 @@ const SKYBLUE: [u8; 4] = [0x87, 0xCE, 0xEB, 0xFF];
 const TERRACOTTA: [u8; 4] = [0xFF, 0xB3, 0x87, 0xFF];
 const TEAL: [u8; 4] = [0x00, 0x87, 0x87, 0xFF];
 
-fn pattern(x: usize, y: usize) -> f64 {
-    let x = x as f64;
-    let y = y as f64;
+const SPHERE_RADIUS: f64 = 1.5;
 
-    let a = 0.5 * (x * x + y * y).sqrt();
-    let b = 0.5 * (x * x - y * y).sqrt();
-    let c = 0.5 * (x * x + y * y).sin();
-    let d = 0.5 * (x * x - y * y).cos();
-
-    a * b * c * d
+fn signed_distance(p: &[f64; 3]) -> f64 {
+    norm(p) - SPHERE_RADIUS
 }
 
-fn draw(frame: &mut [u8]) {
+fn sphere_trace(orig: [f64; 3], dir: &[f64; 3]) -> bool {
+
+    let mut p = orig;
+    for _ in 0..100 {
+        let d = signed_distance(&p);
+        if d < 0.001 {
+            return true;
+        }
+        p = plus(&p, &scale(dir, d));
+    }
+
+    false
+}
+
+struct Camera {
+    eye: [f64; 3],
+    look_at: [f64; 3],
+    up: [f64; 3],
+    fov: f64,
+    // cached view vectors
+    view_dir: [f64; 3],
+    view_right: [f64; 3],
+    view_up: [f64; 3],
+}
+
+fn new_camera(eye: [f64; 3], look_at: [f64; 3], up: [f64; 3], fov: f64) -> Camera {
+    let mut cam = Camera {
+        eye,
+        look_at,
+        up,
+        fov,
+        view_dir: [0.0; 3],
+        view_right: [0.0; 3],
+        view_up: [0.0; 3],
+    };
+    recalculate_view(&mut cam);
+    cam
+}
+
+fn recalculate_view(cam: &mut Camera) {
+    let eye = cam.eye;
+    let look_at = cam.look_at;
+    let up = cam.up;
+
+    let dir = minus(&look_at, &eye);
+    let dir = normalize(&dir);
+
+    let right = normalize(&cross(&dir, &up));
+    let up = normalize(&cross(&right, &dir));
+
+    cam.view_dir = dir;
+    cam.view_right = right;
+    cam.view_up = up;
+}
+
+fn cast(cam: &Camera, x: f64, y: f64) -> [f64; 3] {
+    let aspect = WIDTH as f64 / HEIGHT as f64;
+    let fov = cam.fov;
+    let dir = cam.view_dir;
+    let right = cam.view_right;
+    let up = cam.view_up;
+
+    let right = scale(&right, aspect * fov.tan());
+    let up = scale(&up, fov.tan());
+
+    let x = x / WIDTH as f64;
+    let y = y / HEIGHT as f64;
+
+    let x = scale(&right, x - 0.5);
+    let y = scale(&up, y - 0.5);
+
+    let dir = plus(&dir, &x);
+    let dir = plus(&dir, &y);
+
+    normalize(&dir)
+}
+
+struct Scene {
+    camera: Camera,
+}
+
+fn draw(frame: &mut [u8], scene: &Scene) {
     for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
         let x = i % WIDTH;
         let y = i / WIDTH;
 
-        let rgba = match pattern(x, y) {
-            x if x < -1000. => SALMON,
-            x if x < 0. => SKYBLUE,
-            x if x < 1000. => TERRACOTTA,
-            _ => TEAL,
+        let eye = scene.camera.eye;
+        let dir = cast(&scene.camera, x as f64, y as f64);
+
+        let fg = if y < HEIGHT / 2 { TERRACOTTA } else { SALMON };
+        let bg = if y < HEIGHT / 2 { SKYBLUE } else { TEAL };
+
+        let rgba = if sphere_trace(eye, &dir) {
+            fg
+        } else {
+            bg
         };
 
         pixel.copy_from_slice(&rgba);
@@ -47,9 +127,19 @@ fn run(
     window: Window,
     mut pixels: Pixels,
 ) -> Result<(), Error> {
+
+    let scene = Scene {
+        camera: new_camera(
+            [0.0, 0.0, 3.0],
+            [0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            std::f64::consts::PI / 3.0,
+        )
+    };
+
     event_loop.run(move |event, _, control_flow| {
         if let Event::RedrawRequested(_) = event {
-            draw(pixels.get_frame_mut());
+            draw(pixels.get_frame_mut(), &scene);
             if let Err(err) = pixels.render() {
                 eprintln!("pixels.render() failed: {err}");
                 *control_flow = ControlFlow::Exit;
@@ -98,4 +188,34 @@ fn main() -> Result<(), Error> {
     };
 
     run(event_loop, input, window, pixels)
+}
+
+
+fn norm(v: &[f64; 3]) -> f64 {
+    (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]).sqrt()
+}
+
+fn minus(v1: &[f64; 3], v2: &[f64; 3]) -> [f64; 3] {
+    [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]
+}
+
+fn plus(v1: &[f64; 3], v2: &[f64; 3]) -> [f64; 3] {
+    [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]]
+}
+
+fn scale(v: &[f64; 3], s: f64) -> [f64; 3] {
+    [v[0] * s, v[1] * s, v[2] * s]
+}
+
+fn normalize(v: &[f64; 3]) -> [f64; 3] {
+    let n = norm(v);
+    [v[0] / n, v[1] / n, v[2] / n]
+}
+
+fn cross(v1: &[f64; 3], v2: &[f64; 3]) -> [f64; 3] {
+    [
+        v1[1] * v2[2] - v1[2] * v2[1],
+        v1[2] * v2[0] - v1[0] * v2[2],
+        v1[0] * v2[1] - v1[1] * v2[0],
+    ]
 }
